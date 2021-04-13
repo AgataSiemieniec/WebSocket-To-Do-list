1ec1bd3921b86fdf6061360e920737a682ffa07f
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-04-13 02:19:08 +0200

    Add listeners to the server: add & remove tasks

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,12 @@ class App extends React.Component {
           <h2>Tasks</h2>
     
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            <li className="task">Shopping <button className="btn btn--red">Remove</button></li>
+            <li className="task">Go out with a dog <button className="btn btn--red">Remove</button></li>
           </ul>
     
           <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+            <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
             <button className="btn" type="submit">Add</button>
           </form>
     
--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const express = require('express');
+const path = require('path');
 const socket = require('socket.io');
 
 const app = express();
+
+const tasks = [];
+console.log('tasks', tasks);
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running...');
   });
 
-  const io = socket(server);
+const io = socket(server);
 
-  app.use((req, res) => {
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    io.to(socket.id).emit('updateData', tasks);
+
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+        console.log('User ' + socket.id + 'just added new task' + task);
+      });
+
+      socket.on('removeTask', (removedTask) => {
+        const removedTask = tasks.find(tasks => tasks.id === socket.id);
+        const taskToRemove= tasks.indexOf(removedTask);
+        if(removedUser){
+            socket.broadcast.emit('removeTask', removedTask);
+            users.splice(taskToRemove, 1);
+            console.log(task + 'has been deleted');
+        }
+      });
+});
+
+app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
 });
 
